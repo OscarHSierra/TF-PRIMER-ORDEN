3d3a50b1343855a527eaabc884d3244b7b9ff427
OscarHSierra <110925231+OscarHSierra@users.noreply.github.com>
2023-02-26 18:58:48 -0500

    Add files via upload

---
+++ TF_FO.js
@@ -0,0 +1,49 @@
+var ts= document.getElementById("input_ts");
+var k= document.getElementById("input_k");
+
+
+var boton=document.getElementById("boton");
+var botonayuda=document.getElementById("botonayuda");
+
+var r=document.getElementById("resultado");
+var r2=document.getElementById("ayuda");
+var ganancia=0.0;
+var a=0.0;
+
+var aux=0;
+
+function calcular(){
+
+    ts.value=parseFloat(ts.value);
+    k.value=parseFloat(k.value);
+
+    if (ts.value > 0 && k.value>0 )
+    {
+        a=1/(ts.value/4);   
+        ganancia=k.value;
+        r.innerHTML="La funcion de transferencia del sistema de primer orden es:"+"<br/>"+"<br/>" + "&nbsp&nbsp&nbsp&nbsp"+ ganancia  +"<br/>"+"G(s)=   ----------------"+"<br/>"+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+"s + "+ a.toFixed(2);
+
+    }
+    else
+    {   
+        r.innerHTML="Error de parametros";
+    }
+
+}
+function ayuda()
+{
+    if(aux==1)
+    {
+        aux=0;
+        r2.innerHTML="";
+
+    }
+    else
+    {
+        aux=aux+1;
+        r2.innerHTML="Este programa entrega la función de transferencia de un sistema de primer orden a partir de unos valores de ganancia y tiempo de estabilización deseados.";
+    }
+    
+}
+botonayuda.addEventListener("click",ayuda);
+boton.addEventListener("click",calcular);
